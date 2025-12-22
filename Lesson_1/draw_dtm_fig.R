4239f8145b8c217fe08e5a5216be6c461a2bdafd
zhorve <znhorve21@gmail.com>
2025-12-22 18:33:40 -0500

    finished lesson 1, adding questions and figure scripts

--- Lesson_1/draw_dtm_fig.R
+++ Lesson_1/draw_dtm_fig.R
@@ -1,4 +1,4 @@
-img <- png::readPNG("Lesson_1/dsm_fig.png")
+img <- png::readPNG("Lesson_1/dtm_fig.png")
 plot(0:1, 0:1, type = "n", xlab = "", ylab = "", axes = FALSE)
 rasterImage(img, 0, 0, 1, 1)
 
